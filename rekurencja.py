bdfabdee51995dfd503a0baef1cbd1a9a999f131
imicadio <mrjesio@gmail.com>
2017-01-03 22:58:03 +0100

    Update rekurencja.py

--- rekurencja.py
+++ rekurencja.py
@@ -2,10 +2,10 @@ wynik = []
 def tablice(listy):
     for i in listy:
         if isinstance(i,list):
-            wynik.append(tablice(i))
-            wynik.pop()
+            tablice(i)
         else:
             wynik.append(i)
     return wynik
+
 
 print(tablice([1, [2, 3], [4, [5, 6, [7, 88]]]]))
